1ccaef8b5882bfb545b424b00f1c008cca97c5df
Bronson Quick <bronson@bronsonquick.com.au>
2018-03-05 18:52:23 +1000

    Fix puppet linting errors

--- modules/mcrypt/manifests/init.pp
+++ modules/mcrypt/manifests/init.pp
@@ -1,15 +1,16 @@
+# A Chassis extensions to add mcrypt to your Chassis server
 class mcrypt (
-	$path = "/vagrant/extensions/mcrypt",
-	$config
+	$config,
+	$path = '/vagrant/extensions/mcrypt'
 ) {
 
 	$php = $config[php]
 
-	if versioncmp( "${$php}", '5.4') <= 0 {
+	if versioncmp( $php, '5.4') <= 0 {
 		$php_package = 'php5'
 	}
 	else {
-		$php_package = "php$php"
+		$php_package = "php${$php}"
 	}
 
 	package { "${$php_package}-mcrypt":
